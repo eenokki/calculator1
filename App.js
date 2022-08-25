2e0a5bfcc29a2189feb25a395286ca901ad7023b
Eero Nikkinen <eenokki@gmail.com>
2022-08-26 01:05:55 +0300

    first part done

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, Text, Button, View } from 'react-native';
 
 export default function App() {
+  
+  const buttonAdd = () => {  
+   setResult(parseInt(text1)+parseInt(text2));
+  }
+
+  const buttonSubstract = () => {  
+    setResult(parseInt(text1)-parseInt(text2));
+  }
+
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
+  const [result, setResult] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result}</Text>
+
+     
+        <TextInput 
+          style={styles.input}
+          placeholder="first number"
+          keyboardType="numeric"
+          onChangeText={text1 => setText1(text1)}
+          value={text1}
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="second number"
+        keyboardType="numeric"
+        onChangeText={text2 => setText2(text2)}
+        value={text2}
+        />
+
+      <View style={styles.button}>
+        <Button onPress={buttonAdd} title="+" />
+        <Button onPress={buttonSubstract} title="-" />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +49,22 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+   //display: 'flex',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input : {
+    width:150, 
+    borderColor: 'gray', 
+    borderWidth: 1 ,  
+    padding: 5
+  },
+  button: {
+    flexDirection: 'row',
+    marginTop: 10,
+    width: 150,
+    justifyContent:'space-evenly', 
+  }
 });
